558dbf71d93b1e9fe6c90a486e18fa2ad45ec209
Oscar Creator <oscar.creator13@gmail.com>
2023-04-16 15:16:47 +0200

    rsync to local folder and changed to use toml as config file

--- lua/rsync/init.lua
+++ lua/rsync/init.lua
@@ -1,3 +1,4 @@
+local toml = require("toml")
 
 local M = {}
 
@@ -9,66 +10,73 @@ local rsync_nvim = vim.api.nvim_create_augroup(
     { clear = true }
 )
 
--- configure current buffer with aucmd for syncing 
-local configure_sync = function()
-
-    -- check for config file
-    -- if defined then create autocmd
-    -- TODO check if au group already exist for buffer, then don't do anything
-    if true then
-        vim.api.nvim_create_autocmd({"BufWritePost"}, {
-            callback = function()
-                M.sync_project()
-            end,
-            group = rsync_nvim,
-            buffer = vim.api.nvim_get_current_buf()
-        })
-    end
-end
-
 local get_config_file = function ()
-    local config_file = vim.fn.findfile(".rsyncrc", ".;")
-    if vim.fn.len(config_file) > 0 then
-        return config_file
+    local config_file_path = vim.fn.findfile(".nvim/rsync.toml", ".;")
+    if vim.fn.len(config_file_path) > 0 then
+        local succeeded, table = pcall(toml.decodeFromFile, config_file_path)
+        if succeeded then
+            print(vim.inspect(table))
+            return config_file_path
+        else
+            print("Error decoding file")
+        end
     end
 end
+
 
 vim.api.nvim_create_autocmd({"BufEnter"}, {
     callback = function()
-        if get_config_file() ~= nil then
-            -- only configure if autocmd not already defined
-            if vim.b.rsync_init == nil then
-                configure_sync()
+        -- only initialize once per buffer
+        if vim.b.rsync_init == nil then
+            if get_config_file() ~= nil then
+                vim.api.nvim_create_autocmd({"BufWritePost"}, {
+                    callback = function()
+                        M.sync_project()
+                    end,
+                    group = rsync_nvim,
+                    buffer = vim.api.nvim_get_current_buf()
+                })
+                -- try to initialize if no config file was present at start
+                vim.b.rsync_init = 1
             end
         end
-        vim.b.rsync_init = 1
     end,
     group = rsync_nvim,
 })
 
 M.sync_project = function ()
     -- todo execute rsync command
-    local res = vim.fn.jobstart('ls', {
-        -- job id, output, "stdout"
-        -- triggered on per line
-        on_stdout = function (c, d, n)
-            print("output" .. vim.inspect(d) .. n)
+    vim.b.rsync_status = nil
+    local res = vim.fn.jobstart('rsync -varze --filter=\':- .gitignore\' . ../copy/', {
+        on_stdout = function (id, output, _)
+            -- TODO save output
+            --print("output" .. vim.inspect(output))
         end,
-        on_stderr = function (c, d, n)
+        on_stderr = function (id, output, _)
             -- skip when function reports no error
-            if vim.inspect(d) ~= vim.inspect({ "" }) then
-                print("error:" .. vim.inspect(d) .. " " .. n)
+            if vim.inspect(output) ~= vim.inspect({ "" }) then
+                -- TODO print output
+                -- print("error:" .. vim.inspect(output))
+                --vim.b.rsync_status = 1
             end
         end,
 
         -- job done executing
-        -- job id, exit code, event type
-        on_exit = function (j, c, t)
-            print("exit:" .. vim.inspect(c))
+        on_exit = function (j, code, _)
+            vim.b.rsync_status = code
+            print("exit:" .. vim.inspect(code))
         end,
         stdout_buffered = true,
         stderr_buffered = true
     })
+
+    if res == -1 then
+        print("command not executable")
+    elseif res == 0 then
+        print("invalid arguments")
+    else
+        print("success")
+    end
 end
 
 return M
