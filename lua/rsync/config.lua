c273d7b8583171489cbff5b893232084673ef2fb
Oscar Creator <oscar.creator13@gmail.com>
2023-04-26 18:14:08 +0200

    Changed to rust toml decoding

--- lua/rsync/config.lua
+++ lua/rsync/config.lua
@@ -1,10 +1,9 @@
-local toml = require("toml")
+local rsync_nvim = require("rsync_nvim")
 local path = require("plenary.path")
 
 local config = {}
 config.values = {}
 
--- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/config.lua
 function config.set_defaults(user_defaults)
     user_defaults = vim.F.if_nil(user_defaults, {})
 
@@ -19,7 +18,7 @@ function config.get_project()
     if vim.fn.len(config_file_path) > 0 then
         -- convert to absolute
         config_file_path = path:new(config_file_path):absolute()
-        local succeeded, table = pcall(toml.decodeFromFile, config_file_path)
+        local succeeded, table = pcall(rsync_nvim.decode_toml, config_file_path)
         if succeeded then
             local project_path = string.sub(config_file_path, 1, -string.len(file_path))
             table["project_path"] = project_path
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use mlua::prelude::*;
+use std::fs;
+use toml::{self, Table};
+
+// Return toml content as lua table. If err an empty table
+fn decode_toml(lua: &Lua, file_path: String) -> LuaResult<LuaTable> {
+    let contents = fs::read_to_string(file_path)?;
+    let toml = lua.create_table()?;
+    if let Ok(table) = contents.parse::<Table>() {
+        for (k, v) in table {
+            let i = lua.to_value(&v)?;
+            toml.raw_set(k, i)?;
+        }
+    }
+    Ok(toml)
+}
+
+#[mlua::lua_module]
+fn rsync_nvim(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("decode_toml", lua.create_function(decode_toml)?)?;
+    Ok(exports)
+}
