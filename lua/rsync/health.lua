3d16729dbeb7e9d82abe38228702a11d2115c386
Oscar Creator <oscar.creator13@gmail.com>
2023-04-27 20:03:01 +0200

    Added health check for older versions or rsync

--- lua/rsync/health.lua
+++ lua/rsync/health.lua
@@ -3,9 +3,21 @@ local M = {}
 local function check_rsync()
     local found = vim.fn.executable("rsync")
     if found == 1 then
-        local version_table = vim.fn.json_decode(vim.fn.system("rsync -V -V"))
-        local version = version_table["version"]
-        vim.health.report_ok(("`rsync` found v%s"):format(version))
+        local version_json = vim.fn.system("rsync -V -V")
+        if vim.v.shell_error == 0 then
+            local version_table = vim.fn.json_decode(version_json)
+            local version = version_table["version"]
+            vim.health.report_ok(("`rsync` found v%s"):format(version))
+        else
+            -- old way of retrieving version
+            local version_text = vim.fn.system("rsync --version")
+            local _, _, version = string.find(version_text, "^([^\n]+)\n")
+            if version == nil then
+                vim.health.report_warn(("`rsync` is installed but could not get version"):format())
+            else
+                vim.health.report_ok(("`rsync` found %s"):format(version))
+            end
+        end
     else
         vim.health.report_error(("`rsync` is not installed"):format())
     end
