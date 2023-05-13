224986726d2ac08ff406b1521f46d5329c22a05e
Oscar Creator <oscar.creator13@gmail.com>
2023-05-13 11:37:25 +0200

    Added more tests

--- lua/rsync/project.lua
+++ lua/rsync/project.lua
@@ -13,7 +13,7 @@ local config_path = ".nvim/rsync.toml"
 --- try find a config file.
 -- @return string, or nil
 -- @return absolute path, or config not found error
-local get_config_file = function()
+local function get_config_file()
     local config_file_path = vim.fn.findfile(config_path, ".;")
     if vim.fn.len(config_file_path) > 0 then
         config_file_path = path:new(config_file_path):absolute()
@@ -25,7 +25,7 @@ end
 --- get project path from config file path.
 -- @param config_file_path a path to the config file
 -- @return the project path
-local get_project_path = function(config_file_path)
+local function get_project_path(config_file_path)
     -- TODO maybe add 2 to sub length
     local project_path = string.sub(config_file_path, 1, -string.len(config_path))
     return project_path
@@ -35,7 +35,7 @@ end
 -- @param config_file_path the path to the config file
 -- @return string, or error
 -- @return a table with config, could not decode rsync.toml error
-local get_config = function(config_file_path)
+local function get_config(config_file_path)
     local succeeded, table = pcall(rsync_nvim.decode_toml, config_file_path)
     if succeeded then
         return table
@@ -47,7 +47,7 @@ end
 --- get project config if present
 -- @return table, nil
 -- @return table with config, nil if not found
-project.get_config_table = function()
+function project.get_config_table()
     local config_file_path = get_config_file()
     -- if project does not contain config file
     if config_file_path == nil then
--- tests/rsync/rsync_spec.lua
+++ tests/rsync/rsync_spec.lua
@@ -1,30 +1,113 @@
 local helpers = require("tests.rsync_helper")
-local rsync = require("rsync")
-
---- create temp folder and files which are good to have
 
 describe("rsync", function()
-    describe("files copied", function()
-        before_each(function()
-            helpers.create_workspace()
-        end)
+    before_each(function()
+        helpers.create_workspace()
+        helpers.mkdir(".nvim")
+    end)
 
-        after_each(function()
-            helpers.cleanup_workspace()
-        end)
+    after_each(function()
+        helpers.cleanup_workspace()
+    end)
+
+    describe("files copied", function()
+        local function setup(code)
+            helpers.write_file(".nvim/rsync.toml", {"remote_path = \"" .. helpers.dest .. "/\""})
+            helpers.write_file("test.c", {"eueueu"})
+            helpers.assert_file_not_copied("test.c")
+
+            code()
+
+            helpers.wait_sync()
+            helpers.assert_file("test.c")
+        end
 
         it("on save", function()
-            helpers.mkdir(".nvim")
-            helpers.write_file(".nvim/rsync.toml", "remote_path = \"" .. helpers.dest .. "\"")
-            helpers.write_file("test.c", "eueueu")
+            setup(function()
+                -- this triggers autocommand
+                vim.cmd.w()
+            end)
+        end)
 
-            assert.matches("scratch/source/test.c", vim.api.nvim_command_output("f"))
-            -- this triggers autocommand
-            vim.cmd.w()
+        it("on RsyncUp", function()
+            setup(function()
+                vim.cmd.RsyncUp()
+            end)
+        end)
+    end)
 
-            local config = rsync.config()
-            vim.fn.jobwait({config["sync_status"]["job_id"]})
+    describe("files ignored", function()
+        local function setup_with_gitignore(code)
+            helpers.write_file(".nvim/rsync.toml", {"remote_path = \"" .. helpers.dest .. "/\""})
+            helpers.write_file(".gitignore", {"should_ignore.txt"})
+            helpers.write_file("should_ignore.txt", {"this file\nshould not be synced"})
+            helpers.write_file("test.c", {"eueueu"})
+
+            helpers.assert_file_not_copied("test.c")
+            helpers.assert_file_not_copied("should_ignore.txt")
+
+            code()
+
+            helpers.wait_sync()
             helpers.assert_file("test.c")
+            helpers.assert_file_not_copied("should_ignore.txt")
+        end
+        it("on save", function()
+            setup_with_gitignore(function ()
+                -- this triggers autocommand
+                vim.cmd.w()
+            end)
+        end)
+
+        it("on RsyncUp", function()
+            setup_with_gitignore(function ()
+                vim.cmd.RsyncUp()
+            end)
+        end)
+    end)
+
+    describe("remote includes", function()
+        local function setup_with_remote_includes(config)
+            helpers.write_file(".nvim/rsync.toml", config)
+
+            helpers.write_file(".gitignore", {"remote_file.h"})
+            helpers.write_file("test.c", {"eueueu"})
+            helpers.assert_file_not_copied("test.c")
+
+            vim.cmd.w()
+            helpers.wait_sync()
+
+            helpers.assert_file("test.c")
+        end
+
+        it("synced with RsyncDown", function()
+            setup_with_remote_includes({
+                "remote_path = \"" .. helpers.dest .. "/\"",
+                "remote_includes = [\"remote_file.h\"]",
+            })
+            helpers.write_remote_file("remote_file.h", {"this file should be able to sync down"})
+            helpers.assert_on_remote_only("remote_file.h")
+
+            vim.cmd.RsyncDown()
+            helpers.wait_sync()
+
+            helpers.assert_files({"test.c", "remote_file.h"})
+        end)
+
+        it("synced with RsyncDown multiple files", function()
+            setup_with_remote_includes({
+                "remote_path = \"" .. helpers.dest .. "/\"",
+                "remote_includes = [\"remote_file.h\", \"remote_file_2\"]",
+            })
+            helpers.write_remote_file("remote_file.h", {"this file should be able to sync down"})
+            helpers.assert_on_remote_only("remote_file.h")
+            helpers.write_remote_file("remote_file_2", {"second file", "with a bunch of text"})
+            helpers.assert_on_remote_only("remote_file_2")
+
+            vim.cmd.RsyncDown()
+            helpers.wait_sync()
+
+            helpers.assert_files({"test.c", "remote_file.h", "remote_file_2"})
         end)
     end)
 end)
