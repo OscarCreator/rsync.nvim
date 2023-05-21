10be65d93731bae7fb2055e8dc2c8259003b4e10
Oscar Creator <oscar.creator13@gmail.com>
2023-05-21 14:49:34 +0200

    Added coverage for tests

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,6 +1,10 @@
 local cur_path = vim.fn.expand("%:p:h")
 vim.env.RSYNC_ROOT = cur_path
 
+if os.getenv('TEST_COV') then
+  require('luacov')
+end
+
 vim.opt.rtp:append(".")
 -- for github action
 vim.opt.rtp:append("plenary.nvim")
