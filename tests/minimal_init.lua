7ed4808a8a5593c535cb9ef6a286a34b3a76f143
Oscar Creator <oscar.creator13@gmail.com>
2023-05-21 14:57:14 +0200

    Fixed lints

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,8 +1,8 @@
 local cur_path = vim.fn.expand("%:p:h")
 vim.env.RSYNC_ROOT = cur_path
 
-if os.getenv('TEST_COV') then
-  require('luacov')
+if os.getenv("TEST_COV") then
+    require("luacov")
 end
 
 vim.opt.rtp:append(".")
