5a79747b845bbdf73d3abec43f25f8295f9a5ce0
Oscar Creator <oscar.creator13@gmail.com>
2023-06-18 09:56:23 +0200

    try: no_mangle

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fs;
 use toml::{self, Table};
 
 // Return toml content as lua table. If err an empty table
+#[no_mangle]
 fn decode_toml(lua: &Lua, file_path: String) -> LuaResult<LuaTable> {
     let contents = fs::read_to_string(file_path)?;
     let toml = lua.create_table()?;
@@ -15,6 +16,7 @@ fn decode_toml(lua: &Lua, file_path: String) -> LuaResult<LuaTable> {
     Ok(toml)
 }
 
+#[no_mangle]
 #[mlua::lua_module]
 fn rsync_nvim(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
