0207f241d67da8174618f1f5affb4a2b65e2ceb2
Oscar Creator <oscar.creator13@gmail.com>
2024-02-04 20:08:22 +0100

    fix: replace deprecated functions

--- lua/rsync/health.lua
+++ lua/rsync/health.lua
@@ -7,19 +7,19 @@ local function check_rsync()
         if vim.v.shell_error == 0 then
             local version_table = vim.fn.json_decode(version_json)
             local version = version_table["version"]
-            vim.health.report_ok(("`rsync` found v%s"):format(version))
+            vim.health.ok(("`rsync` found v%s"):format(version))
         else
             -- old way of retrieving version
             local version_text = vim.fn.system("rsync --version")
             local _, _, version = string.find(version_text, "^([^\n]+)\n")
             if version == nil then
-                vim.health.report_warn(("`rsync` is installed but could not get version"):format())
+                vim.health.warn(("`rsync` is installed but could not get version"):format())
             else
-                vim.health.report_ok(("`rsync` found %s"):format(version))
+                vim.health.ok(("`rsync` found %s"):format(version))
             end
         end
     else
-        vim.health.report_error(("`rsync` is not installed"):format())
+        vim.health.error(("`rsync` is not installed"):format())
     end
 end
 
@@ -27,9 +27,9 @@ local function check_cargo()
     local found = vim.fn.executable("cargo")
     if found == 1 then
         local version = vim.fn.system("cargo -V")
-        vim.health.report_ok(("`cargo` found %s"):format(version))
+        vim.health.ok(("`cargo` found %s"):format(version))
     else
-        vim.health.report_error(("`cargo` is not installed"):format(), { ("Install cargo"):format() })
+        vim.health.error(("`cargo` is not installed"):format(), { ("Install cargo"):format() })
     end
 end
 
